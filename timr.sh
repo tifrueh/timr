5edac439b2c73335d3eecf393e33b6e8a1b9d4d1
Timo Früh <dev@tifrueh.ch>
2026-01-22 22:59:43 +0100

    Further improve style
    Always use quotes and braces for variable references.

--- timr.sh
+++ timr.sh
@@ -29,7 +29,7 @@ time_ss_regex='[0-9][0-9]*:[0-5][0-9]:\([0-5][0-9]\)'
 
 # Display help if requested.
 if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
-    printf '%s' "$help"
+    printf '%s' "${help}"
     exit 0
 fi
 
@@ -43,12 +43,12 @@ fi
 
 # Display help if number of arguments isn't correct.
 if [ $# -lt 1 ] || [ $# -gt 3 ]; then
-    printf '%s' "$help"
+    printf '%s' "${help}"
     exit 1
 fi
 
 # Test if time format is correct.
-if ! expr "$1" : "$time_regex" 1> /dev/null 2>&1; then
+if ! expr "$1" : "${time_regex}" 1> /dev/null 2>&1; then
     printf 'error: expected time format HH+:MM:SS, got %s\n' "$1"
     exit 1
 fi
@@ -68,33 +68,33 @@ else
 fi
 
 # Calculate time in seconds.
-time_hh=$( expr "$1" : "$time_hh_regex" )
-time_mm=$( expr "$1" : "$time_mm_regex" )
-time_ss=$( expr "$1" : "$time_ss_regex" )
+time_hh=$( expr "$1" : "${time_hh_regex}" )
+time_mm=$( expr "$1" : "${time_mm_regex}" )
+time_ss=$( expr "$1" : "${time_ss_regex}" )
 time_s=$(( time_ss + 60*time_mm + 3600*time_hh ))
 now_time=$(date +%s)
 end_time=$(( now_time + time_s ))
 
 # Spin for the specified amount.
-while rest_time=$(( end_time - $(date +%s) )) && [ $rest_time -ge 0 ]; do
-    [ $quiet -eq 1 ] && continue
+while rest_time=$(( end_time - $(date +%s) )) && [ "${rest_time}" -ge 0 ]; do
+    [ "${quiet}" -eq 1 ] && continue
     rest_time_hh=$(( rest_time / 3600 ))
     rest_time_mm=$(( (rest_time % 3600) / 60 ))
     rest_time_ss=$(( (rest_time % 3600) % 60 ))
-    printf "\r%$(( COLUMNS - 6 ))d:%02d:%02d" "$rest_time_hh" "$rest_time_mm" "$rest_time_ss"
-    [ $rest_time -eq 0 ] && break
+    printf "\r%$(( COLUMNS - 6 ))d:%02d:%02d" "${rest_time_hh}" "${rest_time_mm}" "${rest_time_ss}"
+    [ "${rest_time}" -eq 0 ] && break
 done
 
 # Print a closing newline if we're not quiet.
-[ $quiet -eq 0 ] && printf '\n'
+[ "${quiet}" -eq 0 ] && printf '\n'
 
 # Notify the terminal.
 tput bel
 
 # Notify the desktop environment.
 notify-send \
-    --app-name="$progname" \
-    --urgency="$urg" \
+    --app-name="${progname}" \
+    --urgency="${urg}" \
     --icon=clock \
     "timr" \
-    "$msg"
+    "${msg}"
